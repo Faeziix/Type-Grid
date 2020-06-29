f4c3d3e6b60d1e4e317252243823ab3ea9159238
Faezix <amirfaezansari@gmail.com>
2020-06-29 14:14:00 +0430

    submit button added + code + result page

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ const button = document.querySelectorAll(".btn");
 let GridTypes = document.querySelectorAll(".TypeName");
 const resetBtn = document.querySelector(".reset");
 let newType = TypeNames;
+const submitBtn = document.getElementById("Submit");
+const restartBtn = document.getElementById("restart");
 
 (async function idk() {
   const type = await (await fetch("./Data2.json")).json();
@@ -29,10 +31,20 @@ let newType = TypeNames;
       let btnName = btns.textContent;
       newType = newType.filter((e) => type[btnName].includes(e));
       filterTypes(newType);
-      if (!newType.length) {
+      if (newType.length === 0) {
         alert(`there is no type with this attributes`);
         newType = TypeNames;
         button.forEach((btns) => btns.classList.remove("btnClick"));
+      }
+      if (newType.length === 1) {
+        submitBtn.addEventListener("click", () => {
+          document.querySelector(".result").classList.add("resultDisplay");
+          let resultText = document.getElementById("result-text");
+          const Name = document.querySelector(".Input-style").value;
+          Name === ""
+            ? (resultText.textContent = `you are ${newType[0]}`)
+            : (resultText.textContent = `${Name} is ${newType[0]}`);
+        });
       }
     });
   });
@@ -47,4 +59,11 @@ resetBtn.addEventListener("click", () => {
   GridTypes.forEach((i) => i.classList.remove("color"));
   newType = TypeNames;
   button.forEach((btns) => btns.classList.remove("btnClick"));
+});
+
+restartBtn.addEventListener("click", () => {
+  GridTypes.forEach((i) => i.classList.remove("color"));
+  newType = TypeNames;
+  button.forEach((btns) => btns.classList.remove("btnClick"));
+  document.querySelector(".result").classList.remove("resultDisplay");
 });
