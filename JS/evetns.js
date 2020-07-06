e347a45b2fd83a09010ee7858efd3b2eede59c06
Faezix <amirfaezansari@gmail.com>
2020-07-06 12:59:03 +0430

    events file minfied

--- JS/evetns.js
+++ JS/evetns.js
@@ -1,13 +1,1 @@
-
-resetBtn.addEventListener("click", () => {
-  GridTypes.forEach((i) => i.classList.remove("color"));
-  newType = TypeNames;
-  button.forEach((btns) => btns.classList.remove("btnClick"));
-});
-
-restartBtn.addEventListener("click", () => {
-  GridTypes.forEach((i) => i.classList.remove("color"));
-  newType = TypeNames;
-  button.forEach((btns) => btns.classList.remove("btnClick"));
-  document.querySelector(".result").classList.remove("resultDisplay");
-});
+resetBtn.addEventListener("click",()=>{GridTypes.forEach(e=>e.classList.remove("color")),newType=TypeNames,button.forEach(e=>e.classList.remove("btnClick"))}),restartBtn.addEventListener("click",()=>{GridTypes.forEach(e=>e.classList.remove("color")),newType=TypeNames,button.forEach(e=>e.classList.remove("btnClick")),document.querySelector(".result").classList.remove("resultDisplay")});
