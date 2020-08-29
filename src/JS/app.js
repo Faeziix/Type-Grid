7481c00db18fce44a62e1f744971edcc79ff6d10
faezix <amirfaezansari@gmail.com>
2020-08-29 17:27:37 +0430

    all js file into one file

--- src/JS/app.js
+++ src/JS/app.js
@@ -1,4 +1,48 @@
-import { types } from "./Data2.js";
+let types = {
+  Direct: ["ESTJ", "ESTP", "ENTJ", "ENFJ", "ISTJ", "ISTP", "INTJ", "INFJ"],
+  Informative: ["ESFJ", "ESFP", "ENTP", "ENFP", "ISFJ", "ISFP", "INTP", "INFP"],
+  Initiating: ["ESTJ", "ESTP", "ENTJ", "ENFJ", "ESFJ", "ESFP", "ENTP", "ENFP"],
+  Responding: ["ISTJ", "ISTP", "INTJ", "INFJ", "ISFJ", "ISFP", "INTP", "INFP"],
+  Control: ["ESTJ", "ESTP", "ENTJ", "ENFJ", "ISFJ", "ISFP", "INTP", "INFP"],
+  Movement: ["ESFJ", "ESFP", "ENTP", "ENFP", "ISTJ", "ISTP", "INTJ", "INFJ"],
+  Affiliative: ["ESTJ", "ESFJ", "ISTJ", "ISFJ", "ENFJ", "ENFP", "INFJ", "INFP"],
+  Pragmatic: ["ESTP", "ESFP", "ISTP", "ISFP", "ENTJ", "ENTP", "INTJ", "INTP"],
+  Abstract: ["ENTJ", "ENTP", "INTJ", "INTP", "ENFJ", "ENFP", "INFJ", "INFP"],
+  Concrete: ["ESTJ", "ESFJ", "ISTJ", "ISFJ", "ESTP", "ESFP", "ISTP", "ISFP"],
+  Systematic: ["ESTJ", "ESFJ", "ISTJ", "ISFJ", "ENTJ", "ENTP", "INTJ", "INTP"],
+  Interest: ["ESTP", "ESFP", "ISTP", "ISFP", "ENFJ", "ENFP", "INFJ", "INFP"],
+  "Se-Ni": ["ESTP", "ESFP", "ISTP", "ISFP", "ENTJ", "INTJ", "ENFJ", "INFJ"],
+  "Si-Ne": ["ESTJ", "ESFJ", "ISTJ", "ISFJ", "ENTP", "INTP", "ENFP", "INFP"],
+  "Ti-Fe": ["ESTP", "ENFJ", "ESFJ", "ENTP", "ISTP", "INFJ", "ISFJ", "INTP"],
+  "Te-Fi": ["ESTJ", "ENTJ", "ESFP", "ENFP", "ISTJ", "INTJ", "ISFP", "INFP"],
+  Crusader: ["ESFJ", "ENTP", "ISFJ", "INTP"],
+  Templar: ["ESTP", "ENFJ", "ISTP", "INFJ"],
+  Wayfarer: ["ENTJ", "ESFP", "INTJ", "ISFP"],
+  Philosopher: ["ESTJ", "ENFP", "ISTJ", "INFP"],
+};
+const TypeNames = [
+"ESTJ",
+"ESTP",
+"ENTJ",
+"ENFJ",
+"ESFJ",
+"ESFP",
+"ENTP",
+"ENFP",
+"ISTJ",
+"ISTP",
+"INTJ",
+"INFJ",
+"ISFJ",
+"ISFP",
+"INTP",
+"INFP",
+];
+
+const resetBtn = document.querySelector(".reset");
+const button = document.querySelectorAll(".btn");
+let GridTypes = document.querySelectorAll(".TypeName");
+
 
 let newType = TypeNames;
 let tempType;
